2063697345c6efd47bc847552b1f47715044fa82
Mohammed Al-Shugaa <hmdoonwork71@gmail.com>
2026-09-16 16:56:50 +0300

    WIP: some fixes

--- kilo.c
+++ kilo.c
@@ -479,7 +479,7 @@ enum HL_Type EditorGetHLType(const HL_Syntax *syntax, const char *word, const si
  * Set every byte of row->hl that corresponds to every character
  * in the row to the right syntax highlight type.
  */
-int EditorUpdateSyntax(Row *row, const HL_Syntax *syntax) {
+int EditorUpdateSyntax(Row *row, const HL_Syntax *syntax, const size_t num_rows) {
     enum HL_Type* tmp = realloc(row->hl, sizeof(enum HL_Type) * row->rsize);
     if (tmp == NULL) return -1;
     row->hl = tmp;
@@ -578,6 +578,13 @@ int EditorUpdateSyntax(Row *row, const HL_Syntax *syntax) {
         p++; i++;
     }
 
+    /* Propagate syntax change to the next row if the
+     * open comment state changed. This may recursively
+     * affect all the following rows in the file. */
+    if (row->idx + 1 < num_rows) {
+        EditorUpdateSyntax(row + 1, syntax, num_rows);
+    }
+
     return 0;
 }
 
@@ -630,7 +637,7 @@ int EditorUpdateRow(const EditorData *e, Row *row) {
     row->render[row->rsize] = '\0';
 
     /* Update the syntax highlighting attributes of the row. */
-    if (EditorUpdateSyntax(row, e->f_info.syntax) == -1) return -1;
+    if (EditorUpdateSyntax(row, e->f_info.syntax, e->f_info.num_rows) == -1) return -1;
 
     return 0;
 }
@@ -675,7 +682,7 @@ void EditorDelRow(EditorData *e, const size_t at) {
     if (at >= e->f_info.num_rows) return;
 
     EditorFreeRow(&e->f_info.rows[at]);
-    memmove(e->f_info.rows + at, e->f_info.rows + at - 1,
+    memmove(e->f_info.rows + at, e->f_info.rows + at + 1,
         sizeof(Row) * (e->f_info.num_rows - at - 1));
 
     for (size_t i = at; i < e->f_info.num_rows - 1; i++) { e->f_info.rows[i].idx--; }
@@ -698,18 +705,18 @@ int EditorRowInsertChar(EditorData *e, Row *row, const size_t at, const int c) {
     memset(row->chars + at, c, 1);
 
     e->f_info.dirty++;
-    EditorMoveCursor(e, ARROW_RIGHT);
     if (EditorUpdateRow(e, row)) return -1;
     return 0;
 }
 
 /* Delete the character at offset 'at' from the specified row. */
 int EditorRowDelChar(EditorData *e, Row *row, const size_t at) {
-    memmove(row->chars + at - 1, row->chars + at, row->size - at); /* Including the null terminator */
+    if (row->size <= at) return 0;
+
+    memmove(row->chars + at, row->chars + at + 1, row->size - at); /* Including the null terminator */
+    e->f_info.dirty++;
     row->size--;
 
-    e->f_info.dirty++;
-    EditorMoveCursor(e, ARROW_LEFT);
     if (EditorUpdateRow(e, row)) return -1;
     return 0;
 }
@@ -736,10 +743,14 @@ int EditorDelChar(EditorData *e) {
 
 /* Inserting a newline is slightly complex as we have to handle inserting a
  * newline in the middle of a line, splitting the line as needed. */
-void EditorInsertNewline(EditorData *e) {}
+void EditorInsertNewline(EditorData *e) {
+
+}
 
 /* Append the string 's' at the end of a row */
-void EditorRowAppendString(EditorData *e, Row *row, char *s, size_t len) {}
+void EditorRowAppendString(EditorData *e, Row *row, char *s, size_t len) {
+
+}
 
 /**
  * Turn the editor rows into a single heap-allocated string.
